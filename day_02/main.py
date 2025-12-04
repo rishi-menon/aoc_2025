bc3938e8fabbd5021be528fc521dacd0b47656ce
Rishi Menon <rmenon@waabi.ai>
2025-12-04 02:20:09 +0000

    day 2

--- day_02/main.py
+++ day_02/main.py
@@ -1,20 +1,13 @@
 import logging
 import math
 import os
-import os.path as op
-import numpy as np
-import torch
+
 import debugpy
-from pathlib import Path
-from matplotlib import cm
 import logging
 import colorlog
 
 
 _logger = logging.getLogger(__name__)
-
-
-
 
 def setup_pretty_logging():
     logging.basicConfig(level=logging.INFO, format="[%(asctime)s][%(filename)s:%(lineno)d][%(levelname)s]: %(message)s")
@@ -25,72 +18,72 @@ def setup_pretty_logging():
         )
     )
 
-def calc_count_smart(values, old_intermediate):
-    cur_pos = 50
-    counter = 0
-    intermediate = []
 
-    for i, value in enumerate(values):
-        new_pos_without_mod = (cur_pos + value)
-        remainder = new_pos_without_mod % 100
-        factor = (new_pos_without_mod - remainder) // 100
-    
-        if factor <= 0 and remainder == 0:
-            factor -= 1
-        if cur_pos == 0 and factor < 0 and remainder != 0:
-            factor += 1
+def get_invalid_ids(start, end):
+    invalid_ids = []
+    for i in range(start, end+1):
+        if check_invalid(i):
+            invalid_ids.append(i)
+    return invalid_ids
 
-        counter += abs(factor)
-        
-        print (f"{i:5d} cur: {cur_pos:5d}, value: {value:5d}, new: {new_pos_without_mod:5d}, clicks: {abs(factor):5d}, cumulative: {counter:5d}")
-        if old_intermediate[i] != counter:
-            a = 1
+def clamp(x, a, b):
+    if x <= a:
+        return a
+    elif x >= b:
+        return b
+    else:
+        return x
 
-        cur_pos = remainder
-    
-    return counter, intermediate
+def split_string(str_num, count):
+    if count == len(str_num):
+        assert False
+        return []
+    if len(str_num) % count != 0:
+        return []
 
-def sign(x):
-    assert x != 0
-    return 1 if x > 0 else -1
+    all_splits = []
+    for i in range(0, len(str_num), count):
+        all_splits.append(str_num[i:i+count])
+    return all_splits
 
-def calc_count(values):
-    cur_pos = 50
-    counter = 0
-    intermediate = []
-    for i, value in enumerate(values):
-        
-        for j in range(abs(value)):
-            cur_pos = (cur_pos + sign(value)) % 100
-            if cur_pos == 0:
-               counter += 1 
-        # print (f"{i:5d} cur: {cur_pos:5d}, value: {value:5d}")
-        intermediate.append(counter)
-    return counter, intermediate
+def check_invalid(num) -> bool:
+    str_num = str(num)
+    if len(str_num) == 1:
+        return False
+
+    max_val = clamp( math.ceil(len(str_num) / 2) + 1, 2, len(str_num))
+    for i in range(1, max_val):
+        splits = split_string(str_num, i)
+        if len(set(splits)) == 1:
+            return True
+
+    return False
 
 def main():
     _logger.info("Start")
-    # file_name = "input.txt"
-    # file_name = "input_base.txt"
-    # file_name = "input_harshit.txt"
-    file_name = "input_simple_2.txt"
+    # file_name = "input_eg.txt"
+    # file_name = "input_simple.txt"
+    file_name = "input_full.txt"
     with open(file_name) as f:
         lines = f.readlines()
     
     values = []
     for line in lines:
-        line = line.rstrip("\n")
-        dir = (1 if line[0].lower() == "r" else -1)
-        num = int(line[1:])
-        
-        assert num != 0
-        values.append(num * dir)
+        slices = line.rstrip("\n").split(",")
+        for s in slices:
+            start, _, end = s.partition("-")
+            values.append((int(start), int(end)))
 
-    counter_1, intermediate_1 = calc_count(values)
-    counter_2, intermediate_2 = calc_count_smart(values, intermediate_1)
+    invalid_ids = []
+    for start, end in values:
+        invalid_ids.extend(get_invalid_ids(start, end))
     
-    print(f"counter_1: {counter_1}, counter_2: {counter_2}")
-    
+    print(f"invalid_ids:")
+    for i in invalid_ids:
+        print(f"  {i}")
+
+    print("")
+    print(f"sum: {sum(invalid_ids)}")
 
 if __name__ == "__main__":
     if os.environ.get("ENABLE_DEBUGPY") is not None:
